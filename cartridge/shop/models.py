d8ae466af99dc6e5e0b2cff3f5b1fd8e20228870
vladimir Kubantsev <vovchik161@gmail.com>
2015-05-24 16:48:38 +0300

    Create ShippingProvider model

--- cartridge/shop/models.py
+++ cartridge/shop/models.py
@@ -19,6 +19,7 @@ from django.utils.translation import (ugettext, ugettext_lazy as _,
 
 from django.utils.encoding import force_text
 from django.utils.encoding import python_2_unicode_compatible
+from hashlib import sha256
 
 from mezzanine.conf import settings
 from mezzanine.core.fields import FileField
@@ -30,6 +31,43 @@ from mezzanine.utils.models import AdminThumbMixin, upload_to
 
 from cartridge.shop import fields, managers
 from cartridge.shop.utils import clear_session
+
+from django.contrib.auth.models import User
+
+
+class ShippingProvider(Displayable, models.Model):
+    """
+    Credential of provider
+    Dealer registering in system as user
+    provider: user of dealer
+    name: name of dealer
+    token_paraphrase: string for generate token. After create token will set blank and string send to dealer
+    token: auth token. Read-only field in admin, can be changed by set new token_paraphrase
+    token_last_update: time and date of last change token. Read-only field, updates when user changed token
+    token_change_reason: reason of changed token
+    token_change_by: user, who changed token. Read-only field, updates when user changed token
+    """
+    provider = models.ForeignKey(User, related_name='provider')
+    name = models.CharField(max_length=200)
+    token_paraphrase = models.CharField(max_length=200, blank=True, null=True, default='')
+    token = models.CharField(max_length=200, blank=True, null=True, default='')
+    token_last_update_date = models.DateTimeField(default=now)
+    token_change_reason = models.CharField(max_length=250, default='')
+    token_change_by = models.ForeignKey(User, related_name='token_change_by')
+
+    class Meta:
+        verbose_name = _("Shipping provider")
+        verbose_name_plural = _("Shipping providers")
+
+    def save(self, *args, **kwargs):
+        self.token = sha256(self.token_paraphrase).hexdigest()
+        self.token_last_update_date = now
+        self.token_paraphrase = ''
+        super(ShippingProvider, self).save(*args, **kwargs)
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ("shipping_provider", (), {"slug": self.slug})
 
 
 class Priced(models.Model):
